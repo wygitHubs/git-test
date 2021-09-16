824c2b1281269c05c193e9992edae39eab90815a
wy <2805638375@qq.com>
2021-09-16 20:28:18 +0800

    第一次提交项目，添加了Service和输出

---
+++ src/main/java/com/baizhi/service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.baizhi.service.impl;
+
+import com.baizhi.entity.User;
+import com.baizhi.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public List<User> queryAll() {
+        System.out.println("push成功");
+        return null;
+    }
+}
