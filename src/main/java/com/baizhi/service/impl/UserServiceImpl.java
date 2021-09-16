6bdcab893da0d5bfd92e29e4e2edd8b5ef2f5efd
wy <2805638375@qq.com>
2021-09-16 20:43:52 +0800

    第一次提交项目，添加了Service和输出sss

--- src/main/java/com/baizhi/service/impl/UserServiceImpl.java
+++ src/main/java/com/baizhi/service/impl/UserServiceImpl.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     @Override
     public List<User> queryAll() {
-        System.out.println("push成功ss");
+        System.out.println("push成功sss");
         return null;
     }
 }
